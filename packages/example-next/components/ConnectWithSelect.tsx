48f5c9ccdc568c770b1c519108e8246399cf2ae5
nheingit <nheindev@gmail.com>
2022-11-30 15:39:46 -0600

    deploying to vercel

--- packages/example-next/components/ConnectWithSelect.tsx
+++ packages/example-next/components/ConnectWithSelect.tsx
@@ -3,6 +3,7 @@ import type { Web3ReactHooks } from '@web3-react/core'
 import { GnosisSafe } from '@web3-react/gnosis-safe'
 import type { MetaMask } from '@web3-react/metamask'
 import { Network } from '@web3-react/network'
+import { Phantom } from '../../phantom'
 import { WalletConnect } from '@web3-react/walletconnect'
 import { useCallback, useState } from 'react'
 import { CHAINS, getAddChainParameters, URLS } from '../chains'
@@ -44,7 +45,7 @@ export function ConnectWithSelect({
   error,
   setError,
 }: {
-  connector: MetaMask | WalletConnect | CoinbaseWallet | Network | GnosisSafe
+  connector: MetaMask | WalletConnect | CoinbaseWallet | Network | GnosisSafe | Phantom
   chainId: ReturnType<Web3ReactHooks['useChainId']>
   isActivating: ReturnType<Web3ReactHooks['useIsActivating']>
   isActive: ReturnType<Web3ReactHooks['useIsActive']>
